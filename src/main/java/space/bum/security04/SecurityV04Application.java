e31e15e827a8c31679c869b8d9d2faa6a7e874d5
osparking <jbpark03@gmail.com>
2024-04-06 11:47:49 +0900

    필요한 종속성 5 가지 추가함

---
+++ src/main/java/space/bum/security04/SecurityV04Application.java
@@ -0,0 +1,13 @@
+package space.bum.security04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityV04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityV04Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/security04/SecurityV04ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.security04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityV04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
